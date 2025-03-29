c9c3e647a6a73332ad65ab93b70576c7a6688df7
Lucas <lucascan.geo@gmail.com>
2025-03-29 19:59:00 -0300

    Front-end creation

--- backend/jest.integration.config.js
+++
@@ -1,7 +0,0 @@
-const baseConfig = require('./jest.config');
-
-module.exports = {
-  ...baseConfig,
-  testMatch: ['**/tests/integration/**/*.test.ts'], // Only integration tests
-  setupFilesAfterEnv: ['./src/tests/setup.integration.ts'], // Specific setup for integration
-};
--- backend/jest.unit.config.js
+++
@@ -1,7 +0,0 @@
-const baseConfig = require('./jest.config');
-
-module.exports = {
-  ...baseConfig,
-  testMatch: ['**/tests/unit/**/*.test.ts'], // Only unit tests
-  setupFilesAfterEnv: ['./src/tests/setup.unit.ts'], // Specific setup for unit tests
-};
